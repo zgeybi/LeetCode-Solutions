5105b3c8b9aa10ea4e6905f1d6e03100c8b9249a
Tim <zgeibi.t@phystech.edu>
2023-04-26 17:55:38 +0300

    added Easy 258

---
+++ Easy Questions/258. Add-Digits/add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  int addDigits(int num) {
+    if (num < 10) {
+      return num;
+    }
+    int sum = 0;
+    while (num != 0) {
+      sum += num % 10;
+      num /= 10;
+      if (num == 0 && sum >= 10) {
+        num = sum;
+        sum = 0;
+      }
+    }
+    return sum;
+  }
+};
