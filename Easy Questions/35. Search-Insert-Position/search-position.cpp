6bce5a6aa2c27e2a70bbd0720634846823e944e8
Tim <zgeibi.t@phystech.edu>
2023-07-30 20:47:04 +0300

    Added Easy 35. Seacrh insert position

---
+++ Easy Questions/35. Search-Insert-Position/search-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  int searchInsert(vector<int>& nums, int target) {
+    int size = nums.size();
+    for (int i = 0; i < size; i++) {
+      if (nums[i] == target) {
+        return i;
+      }
+      if (nums[i] > target) {
+        return i;
+      } else if (i == size - 1) {
+        return size;
+      }
+    }
+    return 0;
+  }
+};
