86e2b87c869bb58700da3ffebb2025052090d995
zgeybi <timzgeyb01@gmail.com>
2024-04-04 21:35:20 +0300

    improved easy 66

--- Easy Questions/66. Plus-One/plus-one.cpp
+++ Easy Questions/66. Plus-One/plus-one.cpp
@@ -1,21 +1,29 @@
 class Solution {
- public:
-  vector<int> plusOne(vector<int>& digits) {
-    int n = digits.size();
-    for (int i = n - 1; i >= 0; i--) {
-      if (i == n - 1) {
-        digits[i]++;
-      }
-      if (digits[i] == 10) {
-        digits[i] = 0;
-        if (i != 0) {
-          digits[i - 1]++;
-        } else {
-          digits.push_back(0);
-          digits[i] = 1;
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int in_hand = 0, i = digits.size() - 1;
+        while (true){
+            if (i != 0) {
+                if (digits[i] == 9) {
+                    digits[i] = 0;
+                    in_hand = 1;
+                    i--;
+                } else {
+                    digits[i] += 1;
+                    break;
+                }
+            } else {
+                if (digits[i] == 9) {
+                    digits.push_back(0);
+                    digits[0] = 1;
+                    break;
+                } else {
+                    digits[0]++;
+                    break;
+                }
+            }
+            
         }
-      }
+        return digits;
     }
-    return digits;
-  }
 };
